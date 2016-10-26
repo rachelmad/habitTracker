b16deead8aa35ed28a91eca75d429800ce0a6e45
rachelmad <rachelvmadrigal@gmail.com>
2016-10-26 17:42:08 -0500

    Changed more indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-	devServer: {
-		port: 3000,
-		inline: true,
-		hot: true,
-		contentBase: "./static"
-	},
-	entry: "./src/App.js",
-	output: {
-		path: "./static",
-		filename: "bundle.js"
-	},
-	watch: true,
-	module: {
-		loaders: [
-			{
-		       test: /\.js$/,
-		       exclude: /node_modules/,
-		       loader: 'babel',
-		       query: {
-		         presets: ['react', 'es2015'] 
-		       }
-		     }
-		]
-	},
-	resolve: {
-		extensions: ['', '.js', '.es6']
-	}
+  devServer: {
+    port: 3000,
+    inline: true,
+    hot: true,
+    contentBase: "./static"
+  },
+  entry: "./src/App.js",
+  output: {
+    path: "./static",
+    filename: "bundle.js"
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+           test: /\.js$/,
+           exclude: /node_modules/,
+           loader: 'babel',
+           query: {
+             presets: ['react', 'es2015'] 
+           }
+         }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.es6']
+  }
 }
